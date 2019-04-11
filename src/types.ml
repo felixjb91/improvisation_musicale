0c7ba9e3ecd5c4183379355320ace57f4eb37365
Rajasugan Mangalathas <rajasugan12@gmail.com>
2019-04-11 16:46:02 +0200

    Declare Do4 and Do4Diese

--- src/types.ml
+++ src/types.ml
@@ -10,3 +10,20 @@ type note_name =
 type alteration =
     Becarre
   | Diese
+
+type note = {
+  nom: note_name;
+  alteration: alteration;
+  octave: int;
+}
+
+let _ = 
+  let do_4 = {
+    nom = Do;
+    alteration = Becarre;
+    octave = 4;
+  } and do_4_diese = {
+    nom = Do;
+    alteration = Diese;
+    octave = 4;
+  } in ()
