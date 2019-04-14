a0c874a4a8ee25f6534eb7a6a11d5fe3ce5fc6fa
Rajasugan Mangalathas <rajasugan12@gmail.com>
2019-04-14 15:48:45 +0200

    Declare notes

--- src/types.ml
+++ src/types.ml
@@ -17,13 +17,13 @@ type note = {
   octave: int;
 }
 
-let _ = 
-  let do_4 = {
-    nom = Do;
-    alteration = Becarre;
-    octave = 4;
-  } and do_4_diese = {
-    nom = Do;
-    alteration = Diese;
-    octave = 4;
-  } in ()
+let note_to_midi = ()
+
+let _ =
+  let do_4 = { nom = Do; alteration = Becarre; octave = 4; } and 
+  do_4_diese = { nom = Do; alteration = Diese; octave = 4; } and 
+  la_2 = { nom = La; alteration = Becarre; octave = 2; } and
+  la_3 = { nom = La; alteration = Becarre; octave = 3; } and
+  la_4 = { nom = La; alteration = Becarre; octave = 4; } and
+  mi3 = { nom = Mi; alteration = Becarre; octave = 3; }
+  in ()
