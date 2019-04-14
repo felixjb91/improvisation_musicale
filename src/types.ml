76086950d99da6990d698ea18bf4e2f4c1f27e12
Félix <felixjb@hotmail.fr>
2019-04-14 15:01:56 +0200

    Exercice 1 Q2 done

--- src/types.ml
+++ src/types.ml
@@ -17,6 +17,23 @@ type note = {
   octave: int;
 }
 
+let noteName_to_string noteName = 
+  match noteName with
+  | Do -> "Do"
+  | Re -> "Re"
+  | Mi -> "Mi"
+  | Fa -> "Fa"
+  | Sol -> "Sol"
+  | La -> "La"
+  | Si -> "Si"
+
+let affiche_Note note = 
+  let (alter:string) = match note.alteration with
+              | Becarre -> ""
+              | Diese -> "#"
+  in
+  Printf.printf "%s%d%s\n" ((noteName_to_string note.nom):string) ((note.octave):int) (alter:string)
+
 let _ = 
   let do_4 = {
     nom = Do;
@@ -26,4 +43,29 @@ let _ =
     nom = Do;
     alteration = Diese;
     octave = 4;
-  } in ()
+  } and la_2 = {
+    nom = La;
+    alteration = Becarre;
+    octave = 2;
+  } and la_3 = {
+    nom = La;
+    alteration = Becarre;
+    octave = 3;
+  }and la_4 = {
+    nom = Do;
+    alteration = Becarre;
+    octave = 4;
+  }and mi_3 = {
+    nom = Mi;
+    alteration = Becarre;
+    octave = 3;
+  }
+  in 
+  let _ = affiche_Note do_4 ; 
+          affiche_Note do_4_diese;
+          affiche_Note la_2 ; 
+          affiche_Note la_3;
+          affiche_Note la_4 ; 
+          affiche_Note mi_3; 
+  in
+  ()
