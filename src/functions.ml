4bb3127465f92fea9f8a0e232350a2e891aab909
Rajasugan Mangalathas <rajasugan12@gmail.com>
2019-04-15 23:53:29 +0200

    Fixed syntax error

--- src/functions.ml
+++ src/functions.ml
@@ -1,5 +1,3 @@
-open Types
-
 (* Play the note *)
 let play_note note duration =
   Graphics.sound (Types.note2freq note) duration
--- src/improvisation_musicale.ml
+++ src/improvisation_musicale.ml
@@ -1,13 +1,15 @@
 open Types
 open Functions
 
-let _ =
-  let do_4 = note_factory Do Becarre 4
+(* Notes to play *)
+let do_4 = note_factory Do Becarre 4
   and do_4_diese = note_factory Do Diese 4
   and la_2 = note_factory La Becarre 2
   and la_3 = note_factory La Becarre 3
   and la_4 = note_factory La Becarre 4
   and mi_3 = note_factory Mi Becarre 3
+
+let _ =
   begin
     Graphics.open_graph " 400x600";
     Graphics.sound 440 1000;
