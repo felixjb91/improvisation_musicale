dadd6a05123dbc0548bba2aaf95784199bffff75
Rajasugan Mangalathas <rajasugan12@gmail.com>
2019-04-26 13:49:41 +0200

    Updated

--- run.sh
+++ run.sh
@@ -5,12 +5,17 @@ echo "Improvisation Musicale"
 if [ $(dpkg-query -W -f='${Status}' ocaml 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
   echo "ocaml not found. Please install ocaml and try again"
-  exit 1
+  sudo apt-get install ocaml
 fi
+
+echo "Installing dependencies..."
+echo "Requires sudo permissions..."
+
+sudo apt-get install opam libcanberra-gtk-module libcanberra-gtk3-module
 
 echo "Installing required libraries..."
 
-opam install dune graphics
+opam install dune graphics lablgtk2
 
 echo "Building the project..."
 
