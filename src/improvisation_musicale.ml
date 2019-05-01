9545dfe07aef17a9ae5e2f882ab3d3e69526dce2
Rajasugan Mangalathas <rajasugan12@gmail.com>
2019-05-01 21:34:18 +0200

    Removed Test scores

--- src/improvisation_musicale.ml
+++ src/improvisation_musicale.ml
@@ -2,18 +2,15 @@ open GMain
 open GdkKeysyms
 open Songs
 
-(* Test Scores MIDI Number *)
-
-let game_of_thrones = [67; 60; 63; 65; 67; 60; 63; 65; 67; 60; 63; 65; 62; 65; 58; 63; 62; 65; 58; 63; 62; 60; 67; 60; 63; 65; 67; 60; 63; 65; 67; 60; 63; 65; 62; 65; 58; 63; 62; 65; 58; 63; 62; 60]
-let duration = [1.0; 0.5; 1.0; 0.75; 1.0; 1.25; 0.5; 0.25; 0.5; 1.0; 2.0; 1.5; 1.25; 2.0; 0.25; 0.5; 1.0; 1.25; 1.0; 0.25; 0.5; 0.25; 1.0; 0.5; 1.25; 0.5; 0.25; 1.0; 1.0; 1.0; 1.25; 0.5; 0.25; 1.0; 0.25; 0.5; 1.0; 0.25; 0.5; 1.0; 2.0; 1.0; 1.0; 0.75; 1.0; 1.0; 0.75; 1.0; 1.0; 1.25; 2.0; 1.0; 1.0; 0.5; 2.0; 1.0; 1.0; 1.25; 0.5; 1.0; 1.0; 1.0; 1.5; 2.0]
-
 let locale = GtkMain.Main.init ()
 
 let main () =
   Sdlmixer.open_audio ();
   at_exit Sdlmixer.close_audio;
   let window = GWindow.window ~width:400 ~height:600 ~title:"Improvisation Musicale" () in
-  let vbox = GPack.vbox ~packing:window#add () in
+  let vbox = GPack.vbox ~packing:window#add
+      ~height:10
+      ~width:10 () in
   let _ = window#connect#destroy ~callback:Main.quit in ();
 
   (* Menu bar *)
@@ -24,17 +21,23 @@ let main () =
 
   (* File menu *)
   let factory = new GMenu.factory file_menu ~accel_group in
-  let _ = factory#add_item "Quit" ~key:_Q ~callback: Main.quit in ();   
+  let _ = factory#add_item "Quit" ~key:_Q ~callback: Main.quit in ();
+
+  (* Horizontal Box *)
+  let hbox = GPack.hbox 
+      ~packing:vbox#add
+      ~height:10
+      ~width:10 () in
 
   (* Play Game of Thrones Theme Music *)
   let button = GButton.button ~label:"Game of thrones"
-      ~packing:vbox#add () in
+      ~packing:hbox#add () in
   let notes_list, duration_list = (Scores.scoreTypedNote2doubleList Samplemusic.game_of_thrones 120.0) in
   let _ = button#connect#clicked ~callback: (fun () -> play_sound notes_list duration_list "Game_of_thrones") in ();
 
   (* Play Ode to Joy Music Simple Version *)
   let button = GButton.button ~label:"Ode to joy!"
-      ~packing:vbox#add () in
+      ~packing:hbox#add () in
   let notes_list, duration_list = (Scores.scoreTypedNote2doubleList Samplemusic.ode_to_joy 120.0) in
   let _ = button#connect#clicked ~callback: (fun () -> play_sound notes_list duration_list "Ode_to_joy") in ();
 
