d7d3612410b9ebab2238d702ecb9d4205621f2a4
3407560 <3407560 alexandregougeon05@gmail.com>
2019-05-01 23:46:12 +0200

    test alex

--- test/tests_notes.ml
+++ test/tests_notes.ml
@@ -5,23 +5,23 @@ open Notes
 let note_1 = note_factory Do Diese 3
 
 let test_note_factory_1 () =
-  Alcotest.(check char) "same nom"  'Do' note_1.nom
-  Alcotest.(check char) "same alter"  'Diese' note_1.alter
-  Alcotest.(check char) "same octave"  '3' note_1.octave
+  Alcotest.(check note_name) "same nom"  Do note_1.nom
+  Alcotest.(check alteration) "same alter"  Diese note_1.alter
+  Alcotest.(check int) "same octave" 3 note_1.octave
 
 let note_2 = note_factory Mi Diese 1
 
 let test_note_factory_2 () =
-  Alcotest.(check char) "same nom"  'Do' note_2.nom
-  Alcotest.(check char) "same alter"  'Becarre' note_2.alter
-  Alcotest.(check int) "same octave"  1 note_2.octave
+  Alcotest.(check note_name) "same nom" Do note_2.nom
+  Alcotest.(check alteration) "same alter" Becarre note_2.alter
+  Alcotest.(check int) "same octave" 1 note_2.octave
 
 (* Test note_factory_type *)
 
 let typed_note = note_factory_type note_1 Noire
 
 let test_note_factory_type_1 () =
-  Alcotest.(check char) "same type_de_note"  'Noire' typed_note.type_de_note
+  Alcotest.(check typed_note) "same type_de_note"  Noire typed_note.type_de_note
 
 (* Test nomNote2num *)
 
@@ -35,7 +35,7 @@ let nomNote2num_1 () =
 let numNote2nom = numNote2nom 5
 
 let numNote2nom_1 () =
-  Alcotest.(check char) "same nom"  'Fa' numNote2nom
+  Alcotest.(check note_name) "same nom"  Fa numNote2nom
 
 (* Test noteType2duree *)
 
@@ -49,7 +49,7 @@ let noteType2duree_1 () =
 let note2midi = note2midi typed_note
 
 let note2midi_1 () =
-  Alcotest.(check float) "same nom" 1 note2midi
+  Alcotest.(check int) "same nom" 1 note2midi
 
 let test_set = [
   "test 1" , `Quick, test_note_factory_1;
